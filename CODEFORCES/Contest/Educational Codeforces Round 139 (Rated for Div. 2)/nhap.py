bfda0094fbc055af2ec68bbfc3941c83af0eaa5c
tranductri2003 <89126960+tranductri2003@users.noreply.github.com>
2022-12-15 21:05:48 +0700

    Add new file

--- CODEFORCES/Contest/Educational Codeforces Round 139 (Rated for Div. 2)/nhap.py
+++ CODEFORCES/Contest/Educational Codeforces Round 139 (Rated for Div. 2)/nhap.py
@@ -1,1 +1,6 @@
-print('a')
+n, m = list(map(int, input().split()))
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+c = a+b
+res = len(set(c))
+print(res)
---
+++ LEETCODE/Problems/(Medium) 1143. Longest Common Subsequence.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):
+        """
+        :type text1: str
+        :type text2: str
+        :rtype: int
+        """
+        dp = []
+        for i in range(len(text1)+1):
+            dp.append([])
+            for j in range(len(text2)+1):
+                dp[i].append(0)
+
+        for i in range(len(text1)):
+            for j in range(len(text2)):
+                if text1[i] == text2[j]:
+                    dp[i+1][j+1] = dp[i][j]+1
+                else:
+                    dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1])
+        return dp[len(text1)][len(text2)]
+
+
+t = Solution()
